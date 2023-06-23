fd7ad5d9f284ea4e455d3ef375af349005603822
Rodrigo Monteiro Roli <33501575+RodrigoMRoli@users.noreply.github.com>
2023-06-23 13:37:25 -0300

    Update controlador/livro
    Foi adicionada uma função para selecionar um livro com um parâmetro ID

--- controladores/livro.js
+++ controladores/livro.js
@@ -1,8 +1,8 @@
-import fs from "fs"
+import { getTodosLivros, getLivroPorId } from "../servicos/livro.js"
 
 function getLivros(req, res) {
     try {
-        const livros = JSON.parse( fs.readFileSync("livros.json") )
+        const livros = getTodosLivros()
         res.send(livros)
     } catch (error) {
         res.status(500)
@@ -10,6 +10,19 @@ function getLivros(req, res) {
     }
 }
 
+function getLivro(req, res) {
+    try {
+        const id = req.params.id
+        const livro = getLivroPorId(id)
+        res.send(livro)
+    } catch (error) {
+        res.status(500)
+        res.send(error.message)
+    }
+}
+
+
 export {
-    getLivros
+    getLivros,
+    getLivro
 }
