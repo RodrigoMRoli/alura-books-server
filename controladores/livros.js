9721a43acf55c6418bb5d5149897d87e27e93bf1
Rodrigo Monteiro Roli <33501575+RodrigoMRoli@users.noreply.github.com>
2023-06-23 11:37:27 -0300

    Update controlador/livro.js
    aprendi o import fs que adiciona diversas funções de arquivos para operar com os arquivos dentro da máquina

--- controladores/livros.js
+++ controladores/livros.js
@@ -1,6 +1,9 @@
+import fs from "fs"
+
 function getLivros(req, res) {
     try {
-        res.send('Olá,   Nice API GET')
+        const livros = JSON.parse( fs.readFileSync("livros.json") )
+        res.send(livros)
     } catch (error) {
         res.status(500)
         res.send(error.message)
