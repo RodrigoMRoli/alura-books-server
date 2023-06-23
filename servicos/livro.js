2f52aeade57de8a4e721577e2fa80ae6c6d8f6ff
Rodrigo Monteiro Roli <33501575+RodrigoMRoli@users.noreply.github.com>
2023-06-23 15:53:36 -0300

    Update servicos/livro.js
    added const livros, funcão de modificar livro pelo id

--- servicos/livro.js
+++ servicos/livro.js
@@ -1,22 +1,30 @@
 import fs from "fs"
 
+const livros = JSON.parse( fs.readFileSync("livros.json") )
+
 function getTodosLivros() {
-    return JSON.parse( fs.readFileSync("livros.json") )
+    return livros
 }
 
 function getLivroPorId(id) {
-    const livros = JSON.parse( fs.readFileSync("livros.json") )
-
     const livroFiltrado = livros.filter(livro => livro.id === id)[0]
     return livroFiltrado
 }
 
 function insereLivro(livroNovo) {
-    const livros = JSON.parse( fs.readFileSync("livros.json") )
-
     const novaListaDeLivros = [ ...livros, livroNovo ]
 
     fs.writeFileSync("livros.json", JSON.stringify())
+}
+
+function modificaLivro(modificacoes, id) {
+    const indiceModificado = livros.findIndex( livro => livro.id === id)
+
+    const conteudoMudado = { ...livros[indiceModificado],  ...modificacoes}
+    // livros[indiceModificado] -> {id:"2", nome: "livro irado"}
+    // modificações -> {nome: "nome mucho legal"}
+
+    
 }
 
 export {
