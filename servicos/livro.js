4f829b8f6d6f18692c91bf4eb6874d06721b7e18
Rodrigo Monteiro Roli <rodrigo.monteiro.roli@gmail.com>
2023-06-26 08:14:31 -0300

    Update serviços/livro
    added deletarLivro.js, implementado função pra incrementar id automático e verificar se id ja existe

--- servicos/livro.js
+++ servicos/livro.js
@@ -12,23 +12,38 @@ function getLivroPorId(id) {
 }
 
 function insereLivro(livroNovo) {
+    const checarNomeRepetido = livros.filter(livro => livro.nome === livroNovo.nome)
+    if (checarNomeRepetido.length > 0) {
+        throw new Error("Esse Livro já existe no banco de dados")
+    }
+    livroNovo.id = Math.max(...livros.map(livro => livro.id)) + 1 // incrementa sempre +1 no tamanho do index
     const novaListaDeLivros = [ ...livros, livroNovo ]
 
-    fs.writeFileSync("livros.json", JSON.stringify())
+    fs.writeFileSync("livros.json", JSON.stringify(novaListaDeLivros))
 }
 
 function modificaLivro(modificacoes, id) {
     const indiceModificado = livros.findIndex( livro => livro.id === id)
-
-    const conteudoMudado = { ...livros[indiceModificado],  ...modificacoes}
+    const conteudoMudado = { ...livros[indiceModificado],  ...modificacoes} // dar uma olhada nessa função (...) depois
     // livros[indiceModificado] -> {id:"2", nome: "livro irado"}
     // modificações -> {nome: "nome mucho legal"}
 
+    livros[indiceModificado] = conteudoMudado
+
+    fs.writeFileSync("livros.json", JSON.stringify(livros))
     
+}
+
+function deletarLivro(id) {
+    delete livros[livros.findIndex( livro => livro.id === id)]
+
+    fs.writeFileSync("livros.json", JSON.stringify(livros))
 }
 
 export {
     getTodosLivros,
     getLivroPorId,
-    insereLivro
+    insereLivro,
+    modificaLivro,
+    deletarLivro
 }
