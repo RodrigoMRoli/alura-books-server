0cfdaca8273569866120fa23c5bbcfa77d6880e7
Rodrigo Monteiro Roli <rodrigo.monteiro.roli@gmail.com>
2023-06-26 08:12:29 -0300

    Update controladores/livro
    added if id is nan, patchLivro e deleteLivro

--- controladores/livro.js
+++ controladores/livro.js
@@ -1,4 +1,4 @@
-import { getTodosLivros, getLivroPorId, insereLivro } from "../servicos/livro.js"
+import { getTodosLivros, getLivroPorId, insereLivro, modificaLivro, deletarLivro } from "../servicos/livro.js"
 
 function getLivros(req, res) {
     try {
@@ -13,8 +13,15 @@ function getLivros(req, res) {
 function getLivro(req, res) {
     try {
         const id = req.params.id
-        const livro = getLivroPorId(id)
-        res.send(livro)
+
+        if (id && Number(id)) { // NaN é naturalmente falso
+            const livro = getLivroPorId(id)
+            res.send(livro)
+        } else {
+            res.status(422)
+            res.send("ID inválido")
+        }
+
     } catch (error) {
         res.status(500)
         res.send(error.message)
@@ -24,9 +31,50 @@ function getLivro(req, res) {
 function postLivro(req, res) {
     try {
         const livroNovo = req.body
-        insereLivro(livroNovo)
-        res.status(201)
-        res.send("Livro inserido com sucesso")
+        
+        if(req.body.nome) {
+            insereLivro(livroNovo)
+            res.status(201)
+            res.send("Livro inserido com sucesso")
+        } else {
+            res.send(422)
+            res.send("O campo nome é obrigatório")
+        }
+    } catch (error) {
+        res.status(500)
+        res.send(error.message)
+    }
+}
+
+function patchLivro(req, res) {
+    try {
+        const id = req.params.id
+        const body = req.body
+
+        if (id && Number(id)) {
+            modificaLivro(body, id)
+            res.send("Item modificado com sucesso")
+        } else {
+            res.status(422)
+            res.send("ID Inválido")
+        }
+    } catch (error) {
+        res.status(500)
+        res.send(error.message)
+    }
+}
+
+function deleteLivro(req, res){
+    try {
+        const id = req.params.id
+
+        if(id && Number(id)) {
+            deletarLivro(id)
+            res.send("Item deletado com sucesso")
+        } else {
+            res.status(422)
+            res.sen("ID Inválido")
+        }
     } catch (error) {
         res.status(500)
         res.send(error.message)
@@ -36,5 +84,7 @@ function postLivro(req, res) {
 export {
     getLivros,
     getLivro,
-    postLivro
+    postLivro,
+    patchLivro,
+    deleteLivro
 }
