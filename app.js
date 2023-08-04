d747e7fe49bacd68d1cc7601dbf0c47138153cc2
Rodrigo Monteiro Roli <rodrigo.monteiro.roli@gmail.com>
2023-08-04 14:20:10 -0300

    npm install

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 import express from "express"
+import cors from 'cors'
 import rotaIndex from "./routes/index.js"
 import rotaLivro from "./routes/livro.js"
 
 const app = express()
 app.use(express.json())
 
-const port = 3000
+const port = 30025
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,PATCH,DELETE');
+    app.use(cors());
+    next();
+});
 
 app.use('/', rotaIndex)
 app.use('/livros', rotaLivro)
